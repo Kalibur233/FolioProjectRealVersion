5a4bca36cd74dd25b869f6821cd76ae2ba0973a7
Kalibur233 <mlii0076@student.monash.edu>
2023-10-16 16:52:14 +1100

    Add project files.

---
+++ FolioProject/Migrations/202310160511241_AppointmentModels.cs
@@ -0,0 +1,33 @@
+﻿namespace FolioProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AppointmentModels : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AppointmentModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        HosiptalId = c.Int(nullable: false),
+                        AppointmentTime = c.DateTime(nullable: false),
+                        Description = c.String(),
+                        MedicImage_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.MedicImages", t => t.MedicImage_Id)
+                .Index(t => t.MedicImage_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AppointmentModels", "MedicImage_Id", "dbo.MedicImages");
+            DropIndex("dbo.AppointmentModels", new[] { "MedicImage_Id" });
+            DropTable("dbo.AppointmentModels");
+        }
+    }
+}
---
+++ FolioProject/Models/AppointmentModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FolioProject.Models
+{
+    public class AppointmentModels
+    {
+        public int Id { get; set; }
+        public MedicImage MedicImage { get; set; }
+        //public int MedicImage MedicImageId { get; set; }
+        //[Required]
+        //public DateTime AppointmentTime { get; set; }
+
+        //public String Description { get; set; }
+
+    }
+
+}
